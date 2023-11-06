1c7a85f74da4e395e1673d1398c981b67700cf59
NandaHardik <149986665+NandaHardik@users.noreply.github.com>
2023-11-06 09:10:07 +0530

    Add files via upload

--- 20A-2.c
+++ 20A-2.c
@@ -5,27 +5,24 @@
 
 struct Book{
     char book_title[30];
-    int Author;
-    int Publication;
+    char Author[30];
+    char Publication[30];
     float Price;
 };
 
 void main(){
-    struct Book b1;
-    char book_title[30];
-    int Author;
-    int Publication;
-    float Price;
+    struct Book b[4];
     int i;
-    for(i=0;i<4;i++){
+    for(i=1;i<5;i++){
     printf("Provide the Tiltle of the Book : ");
-    scanf("%c",b1.book_title);
+    scanf("%s",b[i].book_title);
     printf("Provide the Author's name of the Book : ");
-    scanf("%d",&b1.Author);
+    scanf("%s",&b[i].Author);
     printf("Provide the Publication of the Book : ");
-    scanf("%d",&b1.Publication);
+    scanf("%s",&b[i].Publication);
     printf("Provide the Price of the Book : ");
-    scanf("%f",&b1.Price);
+    scanf("%f",&b[i].Price);
+    printf("%s \n%s \n%s \n%f \n",b[i].book_title,b[i].Author,b[1].Publication,b[1].Price);
     }
 
 }
